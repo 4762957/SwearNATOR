6cc891ee99393f8f5e9c2b0ddcdcdffc79556967
4762957 <64352109+4762957@users.noreply.github.com>
2022-11-06 18:19:19 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-var UwUInput = document.getElementById("input-id");
+var SwearInput = document.getElementById("input-id");
 var Button = document.getElementById("buttonSumbit");
 
-function UwUGenerator(stringForUwU) {
-    var Words = stringForUwU.toString().split(" ");
-    var OwOs = ["UwU", "OwO", "QwQ", "oWo", "OvO", ">W<", "^w^"]
+function SwearGenerator(stringForSwear) {
+    var Words = stringForSwear.toString().split(" ");
+    var Swears = ["UwU", "OwO", "QwQ", "oWo", "OvO", ">W<", "^w^"]
     var RandomWordPosition = Math.floor(Math.random() * Words.length)
-    var RandomOwos = Math.floor(Math.random() * OwOs.length)
-    Words.splice(RandomWordPosition, 0, OwOs[RandomOwos])
+    var RandomSwears = Math.floor(Math.random() * Swears.length)
+    Words.splice(RandomWordPosition, 0, Swears[RandomSwears])
     var Result = Words.join(' ')
     return Result;
 }
 
 Button.onclick = function() {
-    UwUInput = document.getElementById("input-id");
-    UwUInput.value = UwUGenerator(UwUInput.value);
+    SwearInput = document.getElementById("input-id");
+    SwearInput.value = SwearGenerator(SwearInput.value);
 };
