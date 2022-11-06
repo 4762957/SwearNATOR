be9d25f2df7785909262546b1b41506f92059a29
4762957 <64352109+4762957@users.noreply.github.com>
2022-11-06 18:29:45 +0400

    updated
    idk

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ var Button = document.getElementById("buttonSumbit");
 
 function SwearGenerator(stringForSwear) {
     var Words = stringForSwear.toString().split(" ");
-    var Swears = ["Блять", "Пиздец", "Пизда", "Хуй", "Бля", "бля", "Сука"]
+    var Swears = ["блять", "пиздец", "пизда", "хуй", "бля", "Бля", "сука", "Блять", "Пиздец", "Пизда", "Хуй", "Сука", "Ебать", "ебать"]
     var RandomWordPosition = Math.floor(Math.random() * Words.length)
     var RandomSwears = Math.floor(Math.random() * Swears.length)
     Words.splice(RandomWordPosition, 0, Swears[RandomSwears])
     var Result = Words.join(' ')
     return Result;
+    console.log = ("Swears has loaded.")
 }
 
 Button.onclick = function() {
     SwearInput = document.getElementById("input-id");
     SwearInput.value = SwearGenerator(SwearInput.value);
+    console.log = ("Swears was generated to text.")
 };
